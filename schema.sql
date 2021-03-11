d4fcdde1b971505006a4ddccf55aba4b8d136ce2
MarieKleinhans <m.kleinhans@tu-braunschweig.de>
2021-03-11 15:30:41 +0100

    database change

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE users(
     email varchar(100),
     first_name varchar(50),
     last_name varchar(50),
-    password_hash varchar(50),
+    password_hash varchar(100),
     gender varchar(50),
     city varchar(50),
     country varchar(50),
@@ -13,7 +13,7 @@ CREATE TABLE loggedin_users(
     email varchar(100),
     first_name varchar(50),
     last_name varchar(50),
-    password varchar(50),
+    password varchar(100),
     gender varchar(50),
     city varchar(50),
     country varchar(50));
@@ -21,6 +21,5 @@ CREATE TABLE loggedin_users(
 CREATE TABLE messages(
     recepient varchar(100),
     sender varchar(100),
-    msg varchar(140)
-    location varchar(100),
-);
+    msg varchar(140),
+    location varchar(100));
