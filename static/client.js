90d4dcc925791b36067f06477bd6818f0b3923b3
MarieKleinhans <m.kleinhans@tu-braunschweig.de>
2021-03-11 22:59:45 +0100

    digest message function

--- static/client.js
+++ static/client.js
@@ -548,9 +548,9 @@ search_user = function(){
 };
 
 
-/*async function digestMessage(message) {
+async function digestMessage(message) {
   const encoder = new TextEncoder();
   const data = encoder.encode(message);
   const hash = await crypto.subtle.digest('SHA-256', data);
   return hash;
-}*/
+}
